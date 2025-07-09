5581e0c643337adde3a889fcd8ae8d09d079de2a
Bokyung <bokyunglee9903@gmail.com>
2025-07-10 08:17:02 +0900

    Add homepage index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+      <div>
+        <h1>Welcome to Woojin Chemical</h1>
+        <p>This is our homepage built with Next.js!</p>
+      </div>
+    );
+  }
+  
