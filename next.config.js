119ae5aa142f92889adf49bbb381b48c316b52ca
Bokyung <bokyunglee9903@gmail.com>
2025-07-10 09:41:12 +0900

    빌드 에러 수정 - 실험적 기능 제거 및 컴포넌트 구조 정리

--- next.config.js
+++ next.config.js
@@ -2,10 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // Vercel 최적화 설정
-  experimental: {
-    optimizeCss: true,
-  },
   // 이미지 도메인 설정 (필요시)
   images: {
     domains: [],
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
-import MainPage from './MainPage';
+import MainPage from '../components/MainPage';
 
 const HomePage = () => {
   return (
